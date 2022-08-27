4c1913934681e021b6786240f9a6b9207042d84e
Valentin Enchev <112176398+ValkaYuh@users.noreply.github.com>
2022-08-27 20:57:27 +0300

    Fixed a small bug

--- WallReq.py
+++ WallReq.py
@@ -2,8 +2,8 @@ import discord
 import requests
 from discord.ext import commands
 
-TOKEN = 'TOKEN'    # Your discord bot token goes here
-ApiKey = 'APIKEY'  # Your wallhaven api key goes here
+TOKEN = ''    # Your discord bot token goes here
+ApiKey = ''   # Your wallhaven api key goes here
 
 intents = discord.Intents.default()
 intents.message_content = True
