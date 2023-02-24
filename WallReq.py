35ea554e1876cb8eeeec1b2dc6f319b4bcfc5e9b
ValkaYuh <valentinenchev16@gmail.com>
2023-02-24 14:31:35 +0200

    Rewrote some stuff and added slash commands!

--- WallReq.py
+++ WallReq.py
@@ -1,6 +1,6 @@
 import discord
 import requests
-from discord.ext import commands
+from discord import app_commands
 
 TOKEN = ''    # Your discord bot token goes here
 ApiKey = ''   # Your wallhaven api key goes here
@@ -9,17 +9,120 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
 
-client = commands.Bot(command_prefix= ">", intents = intents)
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents = intents)
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"We have logged in as {self.user}.")
+
+
+client = aclient()
+tree = app_commands.CommandTree(client)
+
+
+def top(category):
+    if category == "people":
+        category = category.replace("people", "001")
+    elif category == "anime":
+        category = category.replace("anime", "010")
+    elif category == "general":
+        category = category.replace("general", "100")
+    elif category == "nsfw":
+        category = category.replace("nsfw", "111&purity=001")
+    elif category == "all":
+        category = category.replace("all", "111")
+    else:
+        msg = "Wrong category, the only categories are: general, people, anime, nsfw and all"
+        return msg
+
+    baselink = 'https://wallhaven.cc/api/v1/'
+    uselink = baselink + 'search?apikey=' + ApiKey + '&sorting=toplist&categories=' + category
+    api = requests.get(f"{uselink}")  # get data from wallhaven
+    response = api.json()
+    if api.status_code != 200:
+        msg = 'Request failed with code:' f'{api.status_code}'  # Check if the request is ok
+
+    msg = response['data'][0]['path']  # get the id of the wallpaper from the json file
+    return msg
+
+
+def random(category):
+    if category == "people":
+        category = category.replace("people", "001")
+    elif category == "anime":
+        category = category.replace("anime", "010")
+    elif category == "general":
+        category = category.replace("general", "100")
+    elif category == "nsfw":
+        category = category.replace("nsfw", "111&purity=001")
+    elif category == "all":
+        category = category.replace("all", "111")
+    else:
+        msg = "Wrong category, the only categories are: general, people, anime, nsfw and all"
+        return msg
+
+    baselink = 'https://wallhaven.cc/api/v1/'
+    uselink = baselink + 'search?apikey=' + ApiKey + '&sorting=random&categories=' + category
+    api = requests.get(f"{uselink}")  # get data from wallhaven
+    response = api.json()
+    if api.status_code != 200:
+        msg = 'Request failed with code:' f'{api.status_code}'  # Check if the request is ok
+
+    msg = response['data'][0]['path']  # get the id of the wallpaper from the json file
+    return msg
+
+
+def search(query):
+    baselink = 'https://wallhaven.cc/api/v1/'
+    uselink = baselink + 'search?apikey=' + ApiKey + '&sorting=random&purity=111&q=' + query
+    api = requests.get(f"{uselink}")  # get data from wallhaven
+    response = api.json()
+    if api.status_code != 200:
+        msg = 'Request failed with code:' f'{api.status_code}'  # Check if the request is ok
+
+    msg = response['data'][0]['path']
+    return msg
+
+
+@tree.command(name = "top", description = "Send the top viewed image of the month, of the specified category.(people, general, anime, nsfw)")
+async def self(interaction: discord.Interaction, category: str):
+    await interaction.response.send_message(top(category))
+
+
+@tree.command(name = "random", description = "Send a random image of the specified category.(people, general, anime, nsfw)")
+async def self(interaction: discord.Interaction, category: str):
+    await interaction.response.send_message(random(category))
+
+
+@tree.command(name = "search", description = "Search for a specific image.")
+async def self(interaction: discord.Interaction, query: str):
+    await interaction.response.send_message(search(query))
+
+
+@tree.command(name = "help", description = "Send a message with instructions on how to use the bot")
+async def self(interaction: discord.Interaction):
+    await interaction.response.send_message("You can use the following commands: \n"
+                                            "/top followed by the category \n"
+                                            "/random followed by the category \n"
+                                            "/search followed by your query \n"
+                                            "Categories: general, people, anime, nsfw, all \n"
+                                            "The top command sends the top viewed image of the month in the specific category, if you choose 'all' it will"
+                                            " show the most viewed image across all categories \n"
+                                            "The random command will send a randomly selected image of the specified category \n"
+                                            "With the search command you can search for any type of image, just like in Goole for example :)")
+
 
 @client.event
 async def on_message(message):
     user_message = str(message.content)
-    wid = ''  # Need them both to be global
-    uselink = ''  # ^^^
+    uselink = ''  # needs to be global
     baselink = 'https://wallhaven.cc/api/v1/'
     msg = user_message.lower()  # Mostly to help shorten the huge if statement needed for the search function
 
@@ -76,9 +179,8 @@ async def on_message(message):
         if api.status_code != 200:
             await message.channel.send('Request failed with code:' f'{api.status_code}')  # Check if the request is ok
 
-        wid = response['data'][0]['id']  # get the id of the wallpaper from the json file
+        msg = response['data'][0]['path']
 
-        msg = 'https://wallhaven.cc/w/' + wid
         await message.channel.send(f'{msg}')  # send msg with the link of the wallpaper
         return
     # ------------------------------ SEARCH -----------------------------------------------------------
@@ -92,12 +194,7 @@ async def on_message(message):
         if api.status_code != 200:
             await message.channel.send('Request failed with code:' f'{api.status_code}')  # Check if the request is ok
 
-        wid = response['data'][0]['id']  # get the id of the wallpaper from the json file
-
-        msg = 'https://wallhaven.cc/w/' + wid
-
-        if wid == '{current_page':       # Just checking if wallhaven actually found the wallpaper that we searched for
-            msg = "I couldn't find a wallpaper matching that description."
+        msg = response['data'][0]['path']
 
         await message.channel.send(f'{msg}')  # send msg with the link of the wallpaper
         return
